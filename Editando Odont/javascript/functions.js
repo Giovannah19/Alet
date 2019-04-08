c404b70bd3d9d8ba3ee21459d020c3e9784dd132
Giovannah19 <31668981+Giovannah19@users.noreply.github.com>
2019-04-08 12:45:20 -0300

    Add files via upload

---
+++ Editando Odont/javascript/functions.js
@@ -0,0 +1,8 @@
+function myFunction() {
+			  var x = document.getElementById("cabecalho");
+			  if (x.className === "header") {
+			    x.className += " responsive";
+			  } else {
+			    x.className = "header";
+			  }
+			}
