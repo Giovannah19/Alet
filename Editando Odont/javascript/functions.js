e0851ce0082721de30279382f8e0038578fcc70b
Giovannah19 <giovannah.oliveira@hotmail.com>
2019-04-13 16:38:10 -0300

    remoção de pasta não utilizada

--- Editando Odont/javascript/functions.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-			  var x = document.getElementById("cabecalho");
-			  if (x.className === "header") {
-			    x.className += " responsive";
-			  } else {
-			    x.className = "header";
-			  }
-			}
